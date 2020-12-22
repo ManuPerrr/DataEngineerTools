0d83b3ff90ab25b2cb9da851f1977271b3683394
Manuel Pereira <manuel.pereira@edu.esiee.fr>
2020-12-22 15:56:05 +0100

    TP 1, 2, 3, 4, 5 + Mise en place des objectifs projet

---
+++ 6Evaluation/Projet/film_title.py
@@ -0,0 +1,10 @@
+import scrapy
+
+class ChurchillQuotesSpider(scrapy.Spider):
+    name = "Titre de film"
+    start_urls = ["https://www.allocine.fr/film/meilleurs/",]
+
+    def parse(self, response):
+        for titre in response.xpath('//h2[@class="meta-title"]'):
+            text_value = titre.xpath('a/text()').extract_first()
+            yield { 'Titre' : text_value }
